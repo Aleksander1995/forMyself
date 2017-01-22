4fc65812fecad647fe0822409b5b6a0ac743517b
Александр Лобов < >
2017-01-23 02:31:05 +0300

    Guidelines

--- AngularLessons/angular2-intro-master/app/app.component.ts
+++ AngularLessons/angular2-intro-master/app/app.component.ts
@@ -1,5 +1,10 @@
 import {Component} from "@angular/core";
 
+const todos=[
+    'Изучить JavaScript',
+    'Изучить Angular 2',
+    'Написать приложение'
+];
 @Component({
     moduleId: module.id,
     selector:'app',
@@ -9,4 +14,5 @@ import {Component} from "@angular/core";
 })
 export class AppComponent{
     title: string = "Angular 2Do";
+    todos=todos;
 }
