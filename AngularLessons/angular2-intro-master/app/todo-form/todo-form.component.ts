d5e3d820debe2cbccc7046cd56302d3c83e71af7
Александр Лобов < >
2017-02-19 14:48:50 +0300

    Decomposition

---
+++ AngularLessons/angular2-intro-master/app/todo-form/todo-form.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core'
+
+@Component({
+    moduleId: module.id,
+    selector:'todo-form',
+    templateUrl:'todo-form.component.html',
+    styleUrls: ['todo-form.component.css']
+})
+export class TodoFormComponent {
+    newTodoTitle:string = "";
+
+    private create() {
+
+      /*  let todo:Todo = new Todo(this.newTodoTitle);
+        this.todos.push(todo);
+        this.newTodoTitle = "";*/
+    }
+}
